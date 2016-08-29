9d8c6a34ea35f2569d802794f5f9c0523de601ff
XhmikosR <xhmikosr@gmail.com>
2016-08-29 14:47:51 +0300

    Many lint tweaks.

--- js/httpobs-utils.js
+++ js/httpobs-utils.js
@@ -1,192 +1,194 @@
-// add a prototype to get the average value of an array
-function average(list) {
-    var sum = 0;
-
-    // let's not divide by zero
-    if (list.length === 0) {
-        return undefined;
-    }
-
-    for (var i = 0; i < list.length; i++) {
-        sum += parseInt(list[i], 10);
-    }
-
-    return sum / list.length;
-}
-
-
-function insertGrade(grade, id) {
+(function() {
     'use strict';
-    var letter = grade.substr(0, 1);  // default case
 
-    // locate the elements on the page
-    var dom_container = $('#' + id + '-grade-container');
-    var dom_letter = $('#' + id + '-grade-letter');
-    var dom_modifier = $('#' + id + '-grade-modifier');
+    // add a prototype to get the average value of an array
+    window.average = function (list) {
+        var sum = 0;
 
-    // set the grade
-    switch (grade) {
-        case 'check-mark':
-            letter = Observatory.utils.character_mappings.checkmark;
-            dom_container.toggleClass('grade-a');
-            break;
-        case 'up-arrow':
-            letter = Observatory.utils.character_mappings.uparrow;
-            dom_container.toggleClass('grade-a');
-            break;
-        case 'x-mark':
-            letter = Observatory.utils.character_mappings.xmark;
-            dom_container.toggleClass('grade-f');
-            break;
-        case 'Insecure':  // TODO: kill all this once the TLS Observatory is returning a correct grade
-            letter = 'F';
-        case 'Old (Backwards Compatible)':
-            dom_container.toggleClass('grade-f');
-            break;
-        case 'Intermediate':
-            letter = Observatory.utils.character_mappings.latini;  // latin capital letter i
-            dom_container.toggleClass('grade-a').toggleClass('grade-i');
-            break;
-        case 'Modern':
-            dom_container.toggleClass('grade-a');
-            break;
-        case 'Non-compliant':
-            letter = '?';
-            dom_container.toggleClass('grade-e');
-            break;
-        default:
-            dom_container.toggleClass('grade-' + letter.toLowerCase()); // set the background color for the grade
-    }
+        // let's not divide by zero
+        if (list.length === 0) {
+            return;
+        }
 
-    dom_letter.html(letter);
-    if (grade.length === 2) {
-        dom_container.toggleClass('grade-with-modifier');
-        dom_modifier.text(grade.substr(1, 1));
+        for (var i = 0; i < list.length; i++) {
+            sum += parseInt(list[i], 10);
+        }
 
-        // CSS is the literal worst
-        if (grade[1] === '+') {
-            switch (letter) {
-                case 'A':
-                    dom_modifier.addClass('grade-with-modifier-narrow');
-                    break;
-                case 'C':
-                    dom_modifier.addClass('grade-with-modifier-wide');
+        return sum / list.length;
+    };
+
+
+    window.insertGrade = function (grade, id) {
+        var letter = grade.substr(0, 1);  // default case
+
+        // locate the elements on the page
+        var dom_container = $('#' + id + '-grade-container');
+        var dom_letter = $('#' + id + '-grade-letter');
+        var dom_modifier = $('#' + id + '-grade-modifier');
+
+        // set the grade
+        switch (grade) {
+            case 'check-mark':
+                letter = window.Observatory.utils.character_mappings.checkmark;
+                dom_container.toggleClass('grade-a');
+                break;
+            case 'up-arrow':
+                letter = window.Observatory.utils.character_mappings.uparrow;
+                dom_container.toggleClass('grade-a');
+                break;
+            case 'x-mark':
+                letter = window.Observatory.utils.character_mappings.xmark;
+                dom_container.toggleClass('grade-f');
+                break;
+            case 'Insecure':  // TODO: kill all this once the TLS Observatory is returning a correct grade
+                letter = 'F';
+            case 'Old (Backwards Compatible)':
+                dom_container.toggleClass('grade-f');
+                break;
+            case 'Intermediate':
+                letter = window.Observatory.utils.character_mappings.latini;  // latin capital letter i
+                dom_container.toggleClass('grade-a').toggleClass('grade-i');
+                break;
+            case 'Modern':
+                dom_container.toggleClass('grade-a');
+                break;
+            case 'Non-compliant':
+                letter = '?';
+                dom_container.toggleClass('grade-e');
+                break;
+            default:
+                dom_container.toggleClass('grade-' + letter.toLowerCase()); // set the background color for the grade
+        }
+
+        dom_letter.html(letter);
+        if (grade.length === 2) {
+            dom_container.toggleClass('grade-with-modifier');
+            dom_modifier.text(grade.substr(1, 1));
+
+            // CSS is the literal worst
+            if (grade[1] === '+') {
+                switch (letter) {
+                    case 'A':
+                        dom_modifier.addClass('grade-with-modifier-narrow');
+                        break;
+                    case 'C':
+                        dom_modifier.addClass('grade-with-modifier-wide');
+                }
+            } else {
+                dom_modifier.addClass('grade-with-modifier-narrow'); // C-, B-, etc.
             }
-        } else {
-            dom_modifier.addClass('grade-with-modifier-narrow'); // C-, B-, etc.
         }
-    }
-}
+    };
 
 
-function insertResults(results, id) {
-    'use strict';
+    window.insertResults = function (results, id) {
+        // Write all the various important parts of the scan into the page
+        var keys = Object.keys(results);
 
-    // Write all the various important parts of the scan into the page
-    var keys = Object.keys(results);
-    for (var i in keys) {
-        var key = keys[i];
+        for (var i in keys) {
+            var key = keys[i];
 
-        // insert in the result
-        if (typeof results[key] === 'string') {
-            $('#' + id + '-' + key).text(results[key]);
-        } else {
-            $('#' + id + '-' + key).html(results[key]);
+            // insert in the result
+            if (typeof results[key] === 'string') {
+                $('#' + id + '-' + key).text(results[key]);
+            } else {
+                $('#' + id + '-' + key).html(results[key]);
+            }
+
         }
-
-    }
-}
+    };
 
 
-function errorResults(error, id) {
-    'use strict';
-    // Set the error text and make it a red bar and remove the stripes and swirlies
-    $('#' + id + '-progress-bar-text').text(error).removeClass('active progress-bar-striped').addClass('progress-bar-danger');
-}
+    window.errorResults = function (error, id) {
+        // Set the error text and make it a red bar and remove the stripes and swirlies
+        $('#' + id + '-progress-bar-text')
+            .text(error)
+            .removeClass('active progress-bar-striped')
+            .addClass('progress-bar-danger');
+    };
 
 
-function showResults(id) {
-    'use strict';
-
-    // simply delete the progress bar and the results should show up
-    $('#' + id + '-progress-bar').remove();
-}
+    window.showResults = function (id) {
+        // simply delete the progress bar and the results should show up
+        $('#' + id + '-progress-bar').remove();
+    };
 
 
-function linkify(url) {  // take a link and return an a href
-    'use strict';
+    window.linkify = function (url) {  // take a link and return an a href
+        var a = document.createElement('a');
 
-    var a = document.createElement('a');
-    a.href = url;
-    a.appendChild(document.createTextNode(url));
+        a.href = url;
+        a.appendChild(document.createTextNode(url));
 
-    return a;
-}
-
-
-function listify(list, force) {  // take an array and turn it into an unordered list, if it's > 1 item
-    'use strict';
-    var force = typeof force !== 'undefined' ? force : false;
-
-    // an empty list simple returns an empty string
-    if (list.length === 0) {
-        return '';
-    }
-
-    // a single item list, without forcing it to return a list, will simply return the text
-    if (list.length === 1 && !force) {
-        return list[0];
-    }
-
-    var ul = document.createElement('ul');
-
-    for (var i = 0; i < list.length; i++) {
-        var li = document.createElement('li');
-        var text = document.createTextNode(list[i]);
-
-        li.appendChild(text);
-        ul.appendChild(li);
-    }
-
-    return(ul);
-}
+        return a;
+    };
 
 
-function tableify(list, table_id) {  // take a list of lists and push it into an existing table
-    var table = document.getElementById(table_id);
-    var tbody = document.createElement('tbody');
-    table.appendChild(tbody);
+    window.listify = function (list, force) {  // take an array and turn it into an unordered list, if it's > 1 item
+        var force = typeof force === 'undefined' ? false : force;
 
-    for (var row = 0; row < list.length; row++) {
-        var tr = document.createElement('tr');
-
-        for (var col = 0; col < list[row].length; col++) {
-            var td = document.createElement('td');
-            td.textContent = list[row][col];
-
-            tr.appendChild(td);
+        // an empty list simple returns an empty string
+        if (list.length === 0) {
+            return '';
         }
 
-        tbody.appendChild(tr);
-    }
-}
+        // a single item list, without forcing it to return a list, will simply return the text
+        if (list.length === 1 && !force) {
+            return list[0];
+        }
 
-function toLocalTime(timeString, format) {
-    var localtime = moment.utc(timeString, format).toDate();
+        var ul = document.createElement('ul');
 
-    return moment(localtime).format('LLL');
-}
+        for (var i = 0; i < list.length; i++) {
+            var li = document.createElement('li');
+            var text = document.createTextNode(list[i]);
+
+            li.appendChild(text);
+            ul.appendChild(li);
+        }
+
+        return ul;
+    };
 
 
-function getQueryParameter(param) {
-    var params = _.chain(location.search ? location.search.slice(1).split('&') : '')
-        .map(function(p) {
-            var kp = p.split('=');
-            return [decodeURI(kp[0]), decodeURI(kp[1])];
-        })
-        .fromPairs()
-        .omit(_.isEmpty)
-        .toJSON();
+    window.tableify = function (list, table_id) {  // take a list of lists and push it into an existing table
+        var table = document.getElementById(table_id);
+        var tbody = document.createElement('tbody');
 
-    return params[param];
-}
+        table.appendChild(tbody);
+
+        for (var row = 0; row < list.length; row++) {
+            var tr = document.createElement('tr');
+
+            for (var col = 0; col < list[row].length; col++) {
+                var td = document.createElement('td');
+
+                td.textContent = list[row][col];
+                tr.appendChild(td);
+            }
+
+            tbody.appendChild(tr);
+        }
+    };
+
+    window.toLocalTime = function (timeString, format) {
+        var localtime = moment.utc(timeString, format).toDate();
+
+        return moment(localtime).format('LLL');
+    };
+
+
+    window.getQueryParameter = function (param) {
+        var params = _.chain(location.search ? location.search.slice(1).split('&') : '')
+            .map(function(p) {
+                var kp = p.split('=');
+
+                return [decodeURI(kp[0]), decodeURI(kp[1])];
+            })
+            .fromPairs()
+            .omit(_.isEmpty)
+            .toJSON();
+
+        return params[param];
+    };
+})();
