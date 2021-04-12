da75389135bbdf5838b0114b40352087169153da
Greg Guthe <gguthe@mozilla.com>
2021-04-12 18:16:51 -0400

    add email security section with GCA third party

--- src/js/third-party/third-party.js
+++ src/js/third-party/third-party.js
@@ -1,6 +1,7 @@
 import constants from '../constants.js';
 import utils from '../utils.js';
 
+import * as GCA from './gca.js';
 import * as HSTSPreload from './hsts-preload.js';
 import * as ImmuniWeb from './immuniweb.js';
 import * as SecurityHeaders from './security-headers.js';
@@ -10,6 +11,7 @@ import * as TLSImirhil from './tls-imirhil.js';
 
 const load = async () => {
   await Promise.all([
+    GCA.load(),
     HSTSPreload.load(),
     ImmuniWeb.load(),
     SecurityHeaders.load(),
@@ -19,4 +21,4 @@ const load = async () => {
 }
 
 
-export default { load, HSTSPreload, ImmuniWeb, SecurityHeaders, SSLLabs, TLSImirhil };
+export default { load, GCA, HSTSPreload, ImmuniWeb, SecurityHeaders, SSLLabs, TLSImirhil };
